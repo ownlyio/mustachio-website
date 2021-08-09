ed270f89b32acb5634669e01de2b6252b72b3b1b
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-08-09 16:47:20 +0800

    Add initial ReactJS files for Mustachio website

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Mustachio by Ownly
+        </p>
+        <a
+          className="App-link"
+          href="https://ownly.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Ownly.io
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
