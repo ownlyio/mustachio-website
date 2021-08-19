09e704ac1b3a93910037e56df73dcf50e1921000
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-08-19 19:27:17 +0800

    Add minting function to testnet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
 import { Button } from 'react-bootstrap'
-import Web3 from "web3"
 import './App.css'
 
-import mustachioAbi from './utils/abi'
+// import web3 from './utils/web3'
+import contract from './utils/contract'
 import connectWallet  from './utils/connectWallet'
 import getCurrentNetwork from './utils/getCurrentNetwork'
 import getCurrentWalletConnected  from './utils/getCurrentWalletConnected'
@@ -16,11 +16,16 @@ function App() {
     const [networkId, setNetworkId] = useState(0)
     const [netStatus, setNetStatus] = useState("")
 
-    // State variables for contract
-    const web3 = new Web3()
-    const contractAbi = mustachioAbi()
-    const contractAddress = "0xD559385dD16Da10e6f52Ad236074E7e5ef08fb2e"
-    const contract = new web3.eth.Contract(contractAbi, contractAddress)
+    // State variables for minting
+    const [isHashed, setIsHashed] = useState(false)
+    const [isSuccess, setIsSuccess] = useState(false)
+    const [txHash, setTxHash] = useState("")
+    const [txError, setTxError] = useState("")
+    const [txData, setTxData] = useState([])
+
+    // Other Variables
+    const rinkebyUrl = "https://rinkeby.etherscan.io/tx/"
+    const bscScanUrl = "https://testnet.bscscan.com/tx/"
 
     // Initialize wallet address and network upon button click
     const initUtils = async () => {
@@ -62,6 +67,29 @@ function App() {
         }
     }
 
+    // Mint
+    const mintMustachio = async () => {
+        const mintPrice = await contract.methods.getMintPrice().call()
+        await contract.methods.createToken().send({
+            from: walletAddress,
+            value: mintPrice,
+            type: '0x2',
+        })
+        .on('transactionHash', function(hash){
+            setIsHashed(true)
+        })
+        .on('error', function(error) {
+            setIsHashed(false)
+            setTxError(error.message)
+        })
+        .then(function(receipt) {
+            setIsHashed(false)
+            setIsSuccess(true)
+            setTxHash(receipt.transactionHash)
+            setTxData(receipt)
+        })
+    }
+
     // Initialize wallet address and network if Metamask is already connected
     // Watches for the listeners' update
     useEffect(() => {
@@ -96,7 +124,29 @@ function App() {
             <p>{network !== "" && networkId !== 0 && `Network: ${network} (ID#: ${networkId})`}</p>
             <p>{netStatus}</p>
 
-            <Button variant="primary" className="text-center mt-4">GET MY MUSTACHIO</Button>
+            {/* change to bscmainnet if deployed */}
+            {/* {network !== "bsctestnet" ? (
+                    <Button variant="primary" className="text-center mt-4" disabled>Please connect to Binance Smart Chain Network (Testnet)</Button>
+                ) : (
+                    <Button variant="primary" className="text-center mt-4" onClick={mintMustachio}>Get My Mustachio</Button>
+                )
+            } */}
+            {network !== "rinkeby" ? (
+                    <Button variant="primary" className="text-center mt-4" disabled>Please connect to Rinkeby</Button>
+                ) : (
+                    <Button variant="primary" className="text-center mt-4" onClick={mintMustachio}>Get My Mustachio</Button>
+                )
+            }
+            {isHashed && (
+                <p>Minting your token. Please wait...</p>
+            )}
+            {txError !== "" && (
+                <p>{txError}</p>
+            )}
+            {isSuccess && (
+                <p>Minting success! Check the transaction <a href={rinkebyUrl + txHash}>here.</a></p>
+                // <p>Minting success! Check the transaction <a href={bscScanUrl + txHash}>here.</a></p>
+            )}
         </div>
     )
 }
