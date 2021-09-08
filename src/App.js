2abaab9b55008e842e1f206e69af2c22f24a8099
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-09-08 14:34:24 +0800

    Added Mint functionality based from old website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css'
+import { useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Modal } from 'react-bootstrap'
+import { faCheckCircle, faExclamationCircle } from '@fortawesome/free-solid-svg-icons'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+// Components
 import Navbar from './components/navbar/Navbar'
 import Footer from './components/footer/Footer'
 import FAQs from './components/faqs/FAQs'
 
+// Utils
+// import contract from './utils/contract'
+import contract from './utils/contractDev'
+import getCurrentNetwork from './utils/getCurrentNetwork'
+import getCurrentWalletConnected  from './utils/getCurrentWalletConnected'
+
+// Images
 import grid from './images/grid.jpg'
 import grid2 from './images/grid2.jpg'
 import mustachioBanner from './images/mustachio_banner.jpeg'
@@ -14,8 +27,163 @@ import mustachioGold from './images/mustachio_gold.png'
 import mustachioLogo from './images/mustachio_logo_no_text.png'
 import mustachios from './images/3_mustachios.png'
 import mustacheBoii from './images/mustache_boii.jpg'
+import metamask from './images/metamask.png'
+import loading from './images/loading-mustachio.gif'
  
 function App() {
+    // State variables for initialization
+    const [walletAddress, setWallet] = useState("")
+    const [status, setStatus] = useState(0)
+    const [network, setNetwork] = useState("")
+    const [netStatus, setNetStatus] = useState(0)
+    const [tokenId, setTokenId] = useState(0)
+
+    // State variables for minting
+    const [txHash, setTxHash] = useState("")
+    const [txError, setTxError] = useState("")
+    const [txData, setTxData] = useState([])
+
+    // Other Variables (Change upon deployment)
+    const explorerUrl = "https://rinkeby.etherscan.io/tx/"
+    // const explorerUrl = "https://etherscan.io/tx/"
+    const contractAddress = "0x421dC2b62713223491Daf075C23B39EF0E340E94" // Rinkeby
+    // const contractAddress = "0x9e7a3A2e0c60c70eFc115BF03e6c544Ef07620E5" // MainNet
+    const openSeaUrl = "https://testnets.opensea.io/assets/" + contractAddress + "/"
+    // const openSeaUrl = "https://opensea.io/assets/" + contractAddress + "/"
+
+    // Modals
+    const [showMetamaskInstall, setShowMetamaskInstall] = useState(false);
+    const handleCloseMetamaskInstall = () => setShowMetamaskInstall(false);
+    const handleShowMetamaskInstall = () => setShowMetamaskInstall(true);
+    const [showWrongNetwork, setShowWrongNetwork] = useState(false);
+    const handleCloseWrongNetwork = () => setShowWrongNetwork(false);
+    const handleShowWrongNetwork = () => setShowWrongNetwork(true);
+    const [showOnProcess, setShowOnProcess] = useState(false);
+    const handleCloseOnProcess = () => setShowOnProcess(false);
+    const handleShowOnProcess = () => setShowOnProcess(true);
+    const [showOnError, setShowOnError] = useState(false);
+    const handleCloseOnError = () => setShowOnError(false);
+    const handleShowOnError = () => setShowOnError(true);
+    const [showOnSuccess, setShowOnSuccess] = useState(false);
+    const handleCloseOnSuccess = () => setShowOnSuccess(false);
+    const handleShowOnSuccess = () => setShowOnSuccess(true);
+    const [showSoldOut, setShowSoldOut] = useState(false);
+    const handleCloseSoldOut = () => setShowSoldOut(false);
+    const handleShowSoldOut = () => setShowSoldOut(true);
+
+    // Initialize wallet address and network upon button click
+    // Then mint afterwards
+    const initUtilsAndMint = async () => {
+        const walletResponse = await getCurrentWalletConnected()
+        const networkResponse = await getCurrentNetwork()
+        setStatus(walletResponse.status)
+        setWallet(walletResponse.address)
+        setNetwork(networkResponse.network)
+        setNetStatus(networkResponse.netStatus)
+
+        if (status === 1) {
+            if (network === "rinkeby") {
+            // if (network === "main") {
+                mintMustachio()
+            } else {
+                handleShowWrongNetwork()
+            }
+        } else if (status === 0) {
+            handleShowMetamaskInstall()
+        }
+    }
+
+    // Event Listener for Metamask Account Change
+    const addWalletListener = () => {
+        if (window.ethereum) {
+            window.ethereum.on("accountsChanged", (accounts) => {
+                if (accounts.length > 0) {
+                    setWallet(accounts[0])
+                    setStatus(1)
+                } else {
+                    setWallet("");
+                    setStatus(2)
+                }
+            })
+        } else {
+          setStatus(0);
+        }
+    }
+
+    // Event Listener for Metamask Network Change
+    const addNetworkListener = () => {
+        if (window.ethereum) {
+            window.ethereum.on('chainChanged', async function(networkIdMM){
+                const networkResponseOnLoad = await getCurrentNetwork(1)
+                setNetwork(networkResponseOnLoad.network)
+                setNetStatus(networkResponseOnLoad.netStatus)
+            });            
+        }
+    }
+
+    // shorten addresses and/or txHashes
+    // const shortenAddress = (address, prefixCount, postfixCount) => {
+    //     let prefix = address.substr(0, prefixCount);
+    //     let postfix = address.substr(address.length - postfixCount, address.length);
+    
+    //     return prefix + "..." + postfix;
+    // };
+
+    // Mint
+    const mintMustachio = async () => {
+        const lastId = await contract.methods.getLastMintedTokenId().call()
+
+        if (lastId < 100) {
+            const mintPrice = await contract.methods.getMintPrice().call()
+            await contract.methods.mintMustachio().send({
+                from: walletAddress,
+                value: mintPrice,
+                type: '0x2',
+            })
+            .on('transactionHash', function(hash){
+                handleShowOnProcess()
+            })
+            .on('error', function(error) {
+                handleCloseOnProcess()
+                handleShowOnError()
+                setTxError(error.message)
+            })
+            .then(async function(receipt) {
+                handleCloseOnProcess()
+                handleShowOnSuccess()
+                setTxHash(receipt.transactionHash)
+                setTxData(receipt)
+
+                // Get TokenID
+                const lastTokenId = await contract.methods.getLastMintedTokenId().call()
+                setTokenId(lastTokenId)
+            })
+        } else {
+            handleShowSoldOut()
+        }
+    } 
+
+    // Initialize wallet address and network if Metamask is already connected
+    // Watches for the listeners' update
+    useEffect(() => {
+        async function initUtilsOnLoad() {
+            const {address, status} = await getCurrentWalletConnected();
+            const {network, netStatus} = await getCurrentNetwork();
+            setWallet(address)
+            setStatus(status)
+            setNetwork(network)
+            setNetStatus(netStatus)
+
+            if (status === 0) {
+                handleShowMetamaskInstall()
+            }
+        }
+
+        initUtilsOnLoad()
+        addWalletListener()
+        addNetworkListener()
+    }, []);
+
     return (
         <Router>
             <div className="app">
@@ -48,7 +216,7 @@ function App() {
                                     <p className="text-justify text-lg font-andes text-white mb-lg-0">You can mint your OWN mustachio, and let's unfold the story behind each mustache</p>
                                 </div>
                                 <div className="m-auto col-lg-3 col-12 offset-lg-1">
-                                    <button id="app-mint-button" className="btn w-100 py-3 btn-custom-1 text-2xl font-w-hermann w-hermann-semibold" type="button">MINT YOURS NOW!</button>
+                                    <button id="app-mint-button" className="btn w-100 py-3 btn-custom-1 text-2xl font-w-hermann w-hermann-semibold" type="button" onClick={initUtilsAndMint}>MINT YOURS NOW!</button>
                                     <button className="btn text-center mt-2 w-100 text-lg font-andes-xlight text-sm text-white how">How to mint?</button>
                                 </div>
                             </div>
@@ -224,7 +392,7 @@ function App() {
                             <div className="row mb-4">
                                 <div className="col-lg-7 col-12">
                                     <h1 className="mb-4 text-white font-w-hermann w-hermann-semibold">The Mustachio Team</h1>
-                                    <p className="text-white text-lg font-andes">The Mustachios are created by a secretive-taciturn local artist hiding under the character <a href="https://twitter.com/BoiiMustache">Mustache Boii</a>. He adopted the character Mustache Boii as his personality. The character resembles the artist himself, a long-haired guy with a nevus in his lower left eye and, of course, the legendary mustache.</p>
+                                    <p className="text-white text-lg font-andes">The Mustachios are created by a secretive-taciturn local artist hiding under the character <a href="https://twitter.com/BoiiMustache">Boii Mustache</a>. He adopted the character Boii Mustache as his personality. The character resembles the artist himself, a long-haired guy with a nevus in his lower left eye and, of course, the legendary mustache.</p>
                                     <p className="text-white text-lg font-andes">With the idea of becoming whoever we want to be, he started creating different mustached beings, and The MustachioVerse was conceived and welcomed the Ownly Team.</p>
                                     <p className="text-white text-lg font-andes">You can check them out below.</p>
                                     <div className="app-team-link-wrap">
@@ -252,6 +420,101 @@ function App() {
                     </div>
                 </div>
                 <Footer />
+
+                {/* Modal for soldout */}
+                <Modal show={showSoldOut} onHide={handleCloseSoldOut} backdrop="static" keyboard={false} size="sm" centered>
+                    <Modal.Body>
+                        <div className="text-center mb-3">
+                            <FontAwesomeIcon color="yellow" size="6x" icon={faExclamationCircle} />
+                        </div>
+                        <p className="app-soldout-modal-content text-center"><b style={{fontSize: "1.5rem"}}>SOLD OUT!</b><br />All 100 Mustachios have gone through The Portal. Watch out for the next generation of mustached beings.</p>
+                    </Modal.Body>
+                    <Modal.Footer className="justify-content-center">
+                        <Button variant="secondary" onClick={handleCloseSoldOut}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal> 
+
+                {/* Modal for No Metamask */}
+                <Modal show={showMetamaskInstall} onHide={handleCloseMetamaskInstall} backdrop="static" keyboard={false} size="sm" centered>
+                    <Modal.Body>
+                        <div className="app-metamask-modal-img">
+                            <img src={metamask} alt="Metamask logo" />
+                        </div>
+                        <p className="app-metamask-modal-content text-center">Metamask is currently not installed</p>
+                    </Modal.Body>
+                    <Modal.Footer className="justify-content-center">
+                        <Button variant="secondary" onClick={handleCloseMetamaskInstall}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={() => window.open("https://metamask.io/download", '_blank').focus()}>
+                            Install Metamask
+                        </Button>
+                    </Modal.Footer>
+                </Modal>     
+
+                {/* Modal for incorrect network */}
+                <Modal show={showWrongNetwork} onHide={handleCloseWrongNetwork} backdrop="static" keyboard={false} size="sm" centered>
+                    <Modal.Body>
+                        <div className="text-center mb-3">
+                            <FontAwesomeIcon color="green" size="6x" icon={faExclamationCircle} />
+                        </div>
+                        <p className="app-network-modal-content text-center">Please connect to Rinkeby network</p>
+                        {/* <p className="app-network-modal-content text-center">Please connect to Ethereum Mainnet</p> */}
+                    </Modal.Body>
+                    <Modal.Footer className="justify-content-center">
+                        <Button variant="secondary" onClick={handleCloseWrongNetwork}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>    
+
+                {/* Modal for pending transaction */}
+                <Modal show={showOnProcess} onHide={handleCloseOnProcess} backdrop="static" keyboard={false} size="sm" centered>
+                    <Modal.Body>
+                        <div className="text-center mb-3">
+                            <img src={loading} alt="Loading..." style={{width: "150px", margin: "0 auto"}} />
+                        </div>
+                        <p className="app-pending-modal-content text-center"><span className="app-loading-big-letter">O</span>, what great honour. Put on your armor and hold your fire, dear friend, for we are minting your Mustachio.</p>
+                    </Modal.Body>
+                </Modal>    
+
+                {/* Modal for error transaction */}
+                <Modal show={showOnError} onHide={handleCloseOnError} backdrop="static" keyboard={false} size="sm" centered>
+                    <Modal.Body>
+                        <div className="text-center mb-3">
+                            <FontAwesomeIcon color="red" size="6x" icon={faExclamationCircle} />
+                        </div>
+                        <p className="app-error-modal-content text-center">Error: {txError}</p>
+                    </Modal.Body>
+                    <Modal.Footer className="justify-content-center">
+                        <Button variant="secondary" onClick={handleCloseOnError}>
+                            Close
+                        </Button>
+                    </Modal.Footer>
+                </Modal>    
+
+                {/* Modal for successful transaction */}
+                <Modal show={showOnSuccess} onHide={handleCloseOnSuccess} backdrop="static" keyboard={false} size="md" centered>
+                    <Modal.Body>
+                        <div className="text-center mb-3">
+                            <FontAwesomeIcon color="green" size="6x" icon={faCheckCircle} />
+                        </div>
+                        <p className="app-success-modal-content text-center">Your Mustachio has been successfully minted! You're ready to join the quest to find the Golden Mustache.</p>
+                    </Modal.Body>
+                    <Modal.Footer className="justify-content-center">
+                        <Button variant="secondary" onClick={handleCloseOnSuccess}>
+                            Close
+                        </Button>
+                        <Button variant="primary" onClick={() => window.open(explorerUrl + txHash, '_blank').focus()}>
+                            View on EtherScan
+                        </Button>
+                        <Button variant="primary" onClick={() => window.open(openSeaUrl + tokenId, '_blank').focus()}>
+                            View on OpenSea
+                        </Button>
+                    </Modal.Footer>
+                </Modal>    
             </div>
         </Router>
     )
