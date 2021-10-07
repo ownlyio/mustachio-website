07f972c33d439953708cbd98f8838cf5fd78d03a
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-10-07 19:31:10 +0800

    Create initial design of Backgrounds Page

--- src/components/backgrounds/Backgrounds.js
+++ src/components/backgrounds/Backgrounds.js
@@ -17,7 +17,7 @@ function Backgrounds() {
                     <img className="w-100 xs-banner d-none" src={grid2} alt="Mustachio Banner" />
                 </section>
 
-                <section id="bg-alt" className="mb-4">
+                <section id="bg-title-desc" className="mb-4">
                     <div className="row mb-4">
                         <div className="col-lg-6 col-12">
                             <h1 className="mb-4 text-white font-w-hermann w-hermann-semibold">The Mustachio Backgrounds</h1>
@@ -27,6 +27,14 @@ function Backgrounds() {
                         <div className="my-lg-auto col-lg-5 col-12 offset-lg-1">
                             <img className="w-100" src={mustachioBanner} alt="Mustachio Banner" />
                         </div>
+                    </div>
+                </section>
+
+                <hr className="gray-line my-5" />
+
+                <section id="bg-collection" className="mb-4">
+                    <div className="row mb-4">
+                        <h1 className="mb-4 text-white font-w-hermann w-hermann-semibold">The Backgrounds</h1>
                     </div>
                 </section>
             </div>
