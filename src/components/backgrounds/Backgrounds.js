37cf4f8d6c54168731decd9fb0cb40c8f87d82d1
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-10-07 17:37:23 +0800

    Add backgrounds component

---
+++ src/components/backgrounds/Backgrounds.js
@@ -0,0 +1,37 @@
+import './Backgrounds.css'
+
+// Utils
+import contractBg from '../../utils/contractBackgroundsDev'
+
+// Images
+import grid from '../../images/grid.jpg'
+import grid2 from '../../images/grid2.jpg'
+import mustachioBanner from '../../images/mustachio_banner.jpeg'
+
+function Backgrounds() {
+    return (
+        <div className="bg">
+            <div className="container">
+                <section className="bg-banner mb-5" id="bg-banner">
+                    <img className="w-100 lg-banner" src={grid} alt="Mustachio Banner" />
+                    <img className="w-100 xs-banner d-none" src={grid2} alt="Mustachio Banner" />
+                </section>
+
+                <section id="bg-alt" className="mb-4">
+                    <div className="row mb-4">
+                        <div className="col-lg-6 col-12">
+                            <h1 className="mb-4 text-white font-w-hermann w-hermann-semibold">The Mustachio Backgrounds</h1>
+                            <p className="text-white text-lg font-andes">Make your Mustachios travel all across the MustachioVerse with these amazing Mustachio NFT Backgrounds.</p>
+                            <p className="text-white text-lg font-andes">Just choose the NFT background that you like and mint it now for 0.1ETH!</p>
+                        </div>
+                        <div className="my-lg-auto col-lg-5 col-12 offset-lg-1">
+                            <img className="w-100" src={mustachioBanner} alt="Mustachio Banner" />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+export default Backgrounds
